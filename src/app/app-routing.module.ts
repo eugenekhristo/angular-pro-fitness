dce543f1c1284af1a6e3e2834ac75f61667e5e09
Eugene Khristo <marteau9292@gmail.com>
2019-01-28 08:19:12 +0300

    First commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import {Routes, RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+
+const ROUTES: Routes = [
+  {path: '', redirectTo: 'auth', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(ROUTES)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// feature modules
+import { AuthModule } from './auth/auth.module';
+
+// containers
 import { AppComponent } from './app.component';
 
+// components
+
+// routes
+
+// app state
+import { Store } from './store';
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, AuthModule],
+  providers: [Store],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
