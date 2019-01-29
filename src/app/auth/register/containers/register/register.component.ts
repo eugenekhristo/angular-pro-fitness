6821c27c331bfdd6285d7f6f28f8f046c7ba515e
Eugene Khristo <marteau9292@gmail.com>
2019-01-29 07:58:11 +0300

    auths components is ready

--- src/app/auth/register/containers/register/register.component.ts
+++ src/app/auth/register/containers/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  registerUser(event: FormGroup): void {
+    console.log(event.value);
   }
-
 }
--- src/app/auth/shared/components/auth-form/auth-form.component.ts
+++ src/app/auth/shared/components/auth-form/auth-form.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-auth-form',
   templateUrl: './auth-form.component.html',
   styleUrls: ['./auth-form.component.scss']
 })
-export class AuthFormComponent implements OnInit {
+export class AuthFormComponent {
+  @Output() submitted = new EventEmitter<FormGroup>();
 
-  constructor() { }
+  form = this.fb.group({
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', Validators.required]
+  });
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {}
+
+  get emailControl() {
+    return this.form.get('email');
   }
 
+  get passwordControl() {
+    return this.form.get('password');
+  }
+
+  get passwordInvalid() {
+    const passControl = this.passwordControl;
+    return passControl.hasError('required') && passControl.touched;
+  }
+
+  onSubmit(): void {
+    if (this.form.valid) {
+      this.submitted.emit(this.form);
+    } else {
+      this.emailControl.markAsTouched();
+      this.passwordControl.markAsTouched();
+    }
+  }
 }
